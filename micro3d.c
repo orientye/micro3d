5930b38eb45784311788bc4319325fc2089943eb
yeds <yeds@mail.jj.cn>
2025-05-07 21:30:43 +0800

    micro3d

--- micro3d.c
+++ micro3d.c
@@ -1,13 +1,18 @@
-typedef struct { 
-	float x, y, z, w; 
+typedef struct {
+	float x, y, z, w;
 } vec4_t;
 
-typedef struct { 
-	float m[4][4]; 
+typedef struct {
+	float m[4][4];
 } matrix_t;
 
-typedef struct { 
-	int width;
-	int height;	
-} device_t;
+typedef struct {
+	matrix_t world;
+	matrix_t view;
+	matrix_t projection;
+} transform_t;
 
+typedef struct {
+	int width;
+	int height;
+} device_t;

0fc52496929fa95aedfb70c0453d5820a601a966
orientye <orientye@qq.com>
2025-05-22 20:34:47 +0800

    micro3d

--- micro3d.c
+++ micro3d.c
@@ -12,11 +12,25 @@ typedef struct {
 	matrix_t projection;
 } transform_t;
 
-typedef struct {
-	float r, g, b;
+struct {
+	union {
+		struct {
+			unsigned char r, g, b, a;
+		};
+		unsigned char raw[4];
+		unsigned int val;
+	};
 } color_t;
 
 typedef struct {
 	int width;
 	int height;
 } device_t;
+
+void pixel(device_t* device, int x, int y, color_t clr) {
+
+}
+
+void line(device_t* device, int x1, int y1, int x2, int y2, color_t clr) {
+
+}
